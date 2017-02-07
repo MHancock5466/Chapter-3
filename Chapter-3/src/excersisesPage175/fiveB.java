8e56b01aa4088f6e73ffd589369f32bcd788ad89
MHancock5466 <mhancock5466@gmail.com>
2017-02-07 14:20:58 -0600

    Add files via upload

--- Chapter-3/src/excersisesPage175/fiveB.java
+++ Chapter-3/src/excersisesPage175/fiveB.java
@@ -7,14 +7,32 @@ package excersisesPage175;
  * @author MH137428
  *
  */
-public class fiveB {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+import java.util.Scanner;
+public class fiveB 
+{
+	
+	public static void main(String[] args) 
+	{
+		String answer;
+		double first, second;
+		
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("Please enter a number");
+		first = keyboard.nextDouble();
+		
+		System.out.println("Please enter another number");
+		second = keyboard.nextDouble();
+		
+		computePercent(first, second);
+		computePercent(second, first);
 	}
 
+	public static void computePercent(double fr, double sc)
+	{
+		double percent1;
+		percent1 = 100*(fr/sc);
+		System.out.println(fr + " is " + percent1 + " percent of " + sc + ".");
+	}
+	
 }
--- Chapter-3/src/excersisesPage175/fourB.java
+++ Chapter-3/src/excersisesPage175/fourB.java
@@ -7,14 +7,41 @@ package excersisesPage175;
  * @author MH137428
  *
  */
-public class fourB {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+import javax.swing.JOptionPane;
+public class fourB 
+{
+	int first;
+	public static void main(String[] args) 
+	{
+		String answer;
+		int first;
+		
+		answer = JOptionPane.showInputDialog(null, "Enter a whole number");
+		first = Integer.parseInt(answer);
+		
+		JOptionPane.showMessageDialog(null, "The number when doubled is " + displayTwiceTheNumber(first) + ". The number plus five is " + 
+				displayNumberPlusFive(first) + ". The number when squared is " + displayNumberSquared(first) + ".");
+	}
+	
+	public static int displayTwiceTheNumber(int frst)
+	{
+		int twice;
+		twice = (2 * frst);
+		return twice;
 	}
 
+	public static int displayNumberPlusFive(int frst)
+	{
+		int five;
+		five = (frst + 5);
+		return five;
+	}
+	
+	public static int displayNumberSquared(int frst)
+	{
+		int squared;
+		squared = frst * frst;
+		return squared;
+	}
+	
 }
